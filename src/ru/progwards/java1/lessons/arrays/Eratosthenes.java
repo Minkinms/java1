bf0d067c58ac3acdc869daeb6739c2ef3ef466bb
Minkinms <Minmc@mail.ru>
2020-10-05 16:07:26 +0300

    Переделана задача про Решето. Обновляю по замечаниям наставника

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class Eratosthenes {
     public static void main(String[] args) {
-        Eratosthenes e = new Eratosthenes(15);
-        System.out.println("Число 0 :" + e.isSimple(0));
+        Eratosthenes e = new Eratosthenes(16);
+        //System.out.println(Arrays.toString(e.sieve));
+       System.out.println("Число 0 :" + e.isSimple(0));
         System.out.println("Число 1 :" + e.isSimple(1));
         System.out.println("Число 2 :" + e.isSimple(2));
         System.out.println("Число 3 :" + e.isSimple(3));
@@ -18,7 +19,7 @@ public class Eratosthenes {
         System.out.println("Число 16 :" + e.isSimple(16));
 
 
-    }
+}
 
     //Свойства класса
     private boolean[] sieve;        //Массив для хранеия значений простое или сложное
@@ -29,25 +30,47 @@ public class Eratosthenes {
         //Размер массива задаем по числу N
          sieve = new boolean[N];
          Arrays.fill(sieve, true);
+         //System.out.println(Arrays.toString(sieve));
          sift();
     }
+
     //Метод для сортировки чисел
-    //Числа будут не проверяться на Простое, а исключаться методом перебора
     private void sift() {
-        for (int i=2; i < sieve.length - 1; i++) {
-            for (int j = 2; j < sieve.length -1; j++) {
-                if ((i * j) <= (sieve.length)) {
-                    sieve[i * j - 1] = false;
+        int p = 2;      //Имя переменной и начальное значение из условия задачи
+        boolean exitWhile = false;      //Вспомогательная переменная
+
+        //Внешний цикл проверки чисел-индексов массива sieve
+        while (p <= sieve.length - 1) {
+            for (int j = 2; j < sieve.length - 1; j++) {        //Цикл для перебора множителей 2р, 3p и т.д.
+                if ((p * j) <= (sieve.length - 1)) {            //Условие и функция для вычеркивания чисел
+                    sieve[p * j] = false;
                 }
             }
+            //System.out.println(Arrays.toString(sieve));
+            for (int i = p + 1; i <= sieve.length - 1; i++) {   //Цикл для поиска следующего незачеркнутого
+                if (sieve[i] == true) {     //Если число-индекс Истина, то значение р обновляется,
+                                            //цикл прерывается и перехожу к вычеркиванию след. группы чисел
+                    p = i;
+                    break;
+                } else {
+                    if (i == sieve.length - 1){   //Если все числа-индексы проверены, цикл while пора заканчивать
+                        exitWhile = true;
+                    }
+                }
+            }
+            //Условие для выхода из бесконечного цикла
+            //Если все числа-индексы проверены и true до конца больше нет, то выходим из цикла
+            if (exitWhile == true){
+                break;
+            }
         }
-        System.out.println(Arrays.toString(sieve));
+
     }
 
     //Метод для возвращения "Простое-составное"
     public boolean isSimple(int n) {
-        if (n > 0 && n <= sieve.length) {      //Исключаю запрос на значение числа из непроверенного ряда
-               return sieve[n-1];
+        if (n <= sieve.length - 1){
+            return sieve[n];
         } else {
             System.out.println("Число " + n + " выходит за границы повренного ряда");
             return false;
