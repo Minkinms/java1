4092a98955ad7454fc7f486be6d7497100bd3a87
Minkinms <Minmc@mail.ru>
2020-12-01 09:43:36 +0300

    Задание 12.3,4 v.1

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -8,12 +8,35 @@ public class Finder {
     public static void main(String[] args) {
         Collection<Integer> numbers = new ArrayList<>();
         numbers.add(3); //index 0
-        numbers.add(1); //index 1
+        numbers.add(4); //index 1
         numbers.add(2); //index 2
-        numbers.add(3); //index 3
+        numbers.add(5); //index 3
         numbers.add(2); //index 4
 
-        findMinSumPair(numbers);
+        Collection<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(1); //index 0
+        numbers1.add(2); //index 1
+        numbers1.add(3); //index 2
+        numbers1.add(4); //index 3
+        numbers1.add(5); //index 4
+
+        Collection<String> names = new ArrayList<>();
+        names.add(new String("Николай"));    //index 0
+        names.add(new String("Ииигорь"));     //index 1
+        names.add(new String("Ииигорь"));    //index 2
+        names.add(new String("Ииигорь"));    //index 3
+        names.add(new String("Ольга"));    //index 4
+        names.add(new String("Надежда"));      //index 5
+        names.add(new String("Надежда"));    //index 6
+        names.add(new String("Надежда"));    //index 7
+//        names.add(new String("Надежда"));    //index 8
+
+
+//        findMinSumPair(numbers);
+//        findLocalMax(numbers);
+//        System.out.println(findSequence(numbers1));
+        System.out.println(findSimilar(names));
+
     }
 
     //Задача 12.2.1
@@ -40,19 +63,51 @@ public class Finder {
     }
 
     //Задача 12.2.2
-    //
+    //Поиск локальных максимумов.
     public static Collection<Integer> findLocalMax(Collection<Integer> numbers){
-        return numbers;
+        List<Integer> outCollection = new ArrayList<>();   //Вспомогательная коллекция
+        if (numbers != null && numbers.size() >= 3) {
+            ArrayList<Integer> inCollection = new ArrayList<>(numbers);      //Вспомогательная коллекция, т.к. класс входного объекта не известен
+            for (int i = 1; i < inCollection.size() - 1; i++){
+                if (inCollection.get(i) > inCollection.get(i-1) && inCollection.get(i) > inCollection.get(i+1)){
+                    outCollection.add(inCollection.get(i));
+                }
+            }
+        }
+        return outCollection;
     }
 
     //Задача 12.2.3
     public static boolean findSequence(Collection<Integer> numbers){
-        return false;
+        ArrayList<Integer> inCollection = new ArrayList<>(numbers);      //Вспомогательная коллекция, т.к. класс входного объекта не известен
+        for (int i = 1; i <= inCollection.size(); i++){
+            if (!inCollection.contains(i)){
+                return false;
+            }
+        }
+        return true;
     }
 
     //Задача 12.2.4
     public static String findSimilar(Collection<String> names){
-        return new String(" ");
+        ArrayList<String> inCollection = new ArrayList<>(names);    //Вспомогательная коллекция, т.к. класс входного объекта не известен
+        int count = 0;                      //счетчик совпадений
+        int max = 0;                        //максимум совпадений
+        String outLine = "";                //Строка возвращаемого ответа
+        for (String name1:inCollection){            //Цикл проверки
+            for (String name2:inCollection){
+                if (name1.equals(name2)){
+                    count++;
+                }else{
+                    if (count > max){
+                        max = count;
+                        outLine = name1 + ":" + count;
+                    }
+                    count = 0;
+                }
+            }
+        }
+        return outLine;
     }
 
 }
