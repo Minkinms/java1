3a3fa58e3a218bf2344e3a8785478320fe3fc761
Minkinms <Minmc@mail.ru>
2020-12-08 17:17:05 +0300

    задание 13, задача 1, 2 v.8

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -56,8 +56,8 @@ public class ProductAnalytics {
 
     //2.12 Создать конструктор
     public ProductAnalytics(List<Shop> shops, List<Product> products) {
-        this.shops = shops;
-        this.products = products;
+//        this.shops = shops;
+//        this.products = products;
 //            if (this.shops != null && this.products != null) {
 //        shopArrayList = new ArrayList<>(shops);
 //        productArrayList = new ArrayList<>(products);
