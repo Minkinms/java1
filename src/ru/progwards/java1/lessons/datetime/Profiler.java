71dd9d40500ee9b7c17d97867beb18bd51bf2020
Minkinms <Minmc@mail.ru>
2021-01-25 16:09:01 +0300

    Задание 16 задача 2. с комментами

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -16,12 +16,10 @@ public class Profiler {
         int s2 = 0;
         int s3 = 0;
 
-
         Profiler profiler = new Profiler();
         StatisticInfo profStatInf = new StatisticInfo();
 
         long start = System.currentTimeMillis();
-//        long start1 = Instant.now().getNano();
 
         profiler.enterSection("Section 1");
         for (int i = 0; i < 100000; i++){
@@ -32,7 +30,6 @@ public class Profiler {
                 profiler.enterSection("Section 3");
                 for (int b = 0; b < 10; b++){
                     s3++;
-
                 }
                 exitSection("Section 3");
             }
@@ -40,32 +37,27 @@ public class Profiler {
         }
         exitSection("Section 1");
         System.out.println("Полное время " + (System.currentTimeMillis() - start) + " мсек");
-//        System.out.println((Instant.now().getNano() - start1)/1000000);
-
 
         for (StatisticInfo stInf:getStatisticInfo())
         System.out.println(stInf);
-
-
     }
 
 
     //Переменные класса
-    public static Map<String, StatisticInfo> statisticInfoMap = new HashMap<>();
-    public static HashMap<Integer, String> depthMap = new HashMap<>();
-//        public static Instant instant = Instant.now();
+    public static Map<String, StatisticInfo> statisticInfoMap = new HashMap<>();    //Хранение информации о секциях с общим доступом
+    public static Map<Integer, String> depthMap = new HashMap<>();  //Дополниетльное хранилище для определения вложенности секций
 
     //2.1 Реализовать методы
     //- войти в профилировочную секцию, замерить время входа.
     public static void enterSection(String name){
         if(statisticInfoMap.containsKey(name)){
             statisticInfoMap.get(name).count += 1;
-            statisticInfoMap.get(name).sectionTime = (int)System.currentTimeMillis();
+            statisticInfoMap.get(name).sectionTime = (int)System.currentTimeMillis();   //Запись времени входа в секцию
         }else {
-            StatisticInfo profStatInf = new StatisticInfo();
+            StatisticInfo profStatInf = new StatisticInfo();        //Создание секции
             profStatInf.sectionName = name;
             profStatInf.sectionTime = (int)System.currentTimeMillis();
-            profStatInf.depthSection = depthMap.size();
+            profStatInf.depthSection = depthMap.size();             //Установка и запись глубины вложенности
             statisticInfoMap.put(name, profStatInf);
             depthMap.put(depthMap.size(), name);
         }
@@ -75,33 +67,29 @@ public class Profiler {
     // Замерить время выхода, вычислить промежуток времени между входом и выходом в миллисекундах.
     public static void exitSection(String name){
         if(statisticInfoMap.containsKey(name)){
-            int i = (int)System.currentTimeMillis() - statisticInfoMap.get(name).sectionTime;
-            statisticInfoMap.get(name).fullTime += i;
-            statisticInfoMap.get(name).selfTime = selfTimeCalc(name);
-
+            int i = (int)System.currentTimeMillis() - statisticInfoMap.get(name).sectionTime;   //Определение времени прохождения секции
+            statisticInfoMap.get(name).fullTime += i;                       //перезапись значения суммарного времени
+            statisticInfoMap.get(name).selfTime = selfTimeCalc(name);       //вычисление чистого времени
         }
     }
 
+    //Метод для вычисления чистого времени секции
     public static int selfTimeCalc(String name){
-        String upperName;
-        int result = 0;
-        if(statisticInfoMap.get(name).depthSection != depthMap.size() - 1){
-            upperName = depthMap.get(statisticInfoMap.get(name).depthSection + 1);
-            result = statisticInfoMap.get(name).fullTime - statisticInfoMap.get(upperName).fullTime;
+        if(statisticInfoMap.get(name).depthSection != depthMap.size() - 1){     //Если секция не последняя
+            String upperName = depthMap.get(statisticInfoMap.get(name).depthSection + 1); //Получаем имя секции на уровень ниже, вложенной
+            return statisticInfoMap.get(name).fullTime - statisticInfoMap.get(upperName).fullTime;
         }else{
-            result = statisticInfoMap.get(name).fullTime;
+            return statisticInfoMap.get(name).fullTime;     //Если последняя просто записываем одинаковое время
         }
-
-        return result;
     }
 
     //2.3 получить профилировочную статистику, отсортировать по наименованию секции
      public static List<StatisticInfo> getStatisticInfo(){
          List<StatisticInfo> resultList = new ArrayList<>();
-         for(var value : statisticInfoMap.entrySet()){
+         for(var value : statisticInfoMap.entrySet()){          //Собираю список из объектов секций
             resultList.add(value.getValue());
          }
-        resultList.sort(new Comparator<StatisticInfo>() {
+        resultList.sort(new Comparator<StatisticInfo>() {           //Сортировка
             @Override
             public int compare(StatisticInfo o1, StatisticInfo o2) {
                 return o1.sectionName.compareTo(o2.sectionName);
