ad930898b97a0abe1d06a8e54742b554c441548e
Minkinms <Minmc@mail.ru>
2021-02-03 17:35:27 +0300

    задание 17 задача 1 с комментариями

--- src/ru/progwards/java1/lessons/files/FileInfo.java
+++ src/ru/progwards/java1/lessons/files/FileInfo.java
@@ -4,20 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.FileTime;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Objects;
 
 public class FileInfo {
 
-
     //Переменные класса
     String file_name;           //имя файла
-//    LocalDateTime file_ldt;     //дата последнего изменения
-    FileTime file_lmt;
-    Date file_lmt_date;
+    Date file_date;             //дата последнего изменения
     Integer file_size;          //размер файла
     Path file_path;             //абсолютный путь к файлу
     String fileLine;            //Содержимое файла
@@ -27,48 +21,48 @@ public class FileInfo {
         File fileInfo = new File(file_path.toString());
         this.file_name = fileInfo.getName();
         this.file_path = file_path;
-//        this.file_size = Integer.valueOf(Files.getAttribute(file_path, "size").toString());
         this.file_size = (int)fileInfo.length();
-//        this.file_lmt = (FileTime)Files.getAttribute(file_path, "lastModifiedTime");
-        file_lmt_date = new Date(fileInfo.lastModified());
+        file_date = new Date(fileInfo.lastModified());
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FileInfo)) return false;
+        FileInfo fileInfo = (FileInfo) o;
+        return Objects.equals(file_name, fileInfo.file_name) &&
+                Objects.equals(file_date, fileInfo.file_date) &&
+                Objects.equals(file_size, fileInfo.file_size) &&
+                checkContent(fileInfo);         //Проверка по содержимому.
+                                                // Выполняю последней, чтобы не считывать файлы лишний раз.
+    }
+
+    //Метод для проверки по содержимому
+    public boolean checkContent(FileInfo fileToCheck){
+            getFileString();
+            fileToCheck.getFileString();
+            return fileLine.equals(fileToCheck.fileLine);
+    }
+
+    //Метод для считывания содержимого файла
+    public void getFileString(){
         try {
             fileLine = Files.readString(this.file_path);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<String> getFileList(){
-        List<String> fileInfoList = new ArrayList<>();
-//        fileInfoList.add(file_name);
-        fileInfoList.add(file_path.toString());
-        return fileInfoList;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;         //Подходит ли к проверке по содержимому?
-        if (!(o instanceof FileInfo)) return false;
-        FileInfo fileInfo = (FileInfo) o;
-        if(!fileLine.equals(fileInfo.fileLine)) return false;
-        return Objects.equals(file_name, fileInfo.file_name) &&
-                Objects.equals(file_lmt_date, fileInfo.file_lmt_date) &&
-                Objects.equals(file_size, fileInfo.file_size);
-    }
-
     @Override
     public int hashCode() {
-        return Objects.hash(file_name, file_lmt, file_size);
+        return Objects.hash(file_name, file_date, file_size);
     }
 
     @Override
     public String toString() {
         return "File{" +
                 "Имя ='" + file_name + '\'' +
-                ", Посл.Изм =" + file_lmt_date +
+                ", Посл.Изм =" + file_date +
                 ", Размер =" + file_size +
                 ", Путь =" + file_path +
                 '}';
--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -14,7 +14,7 @@ public class FindDuplicates {
         String startPath = "C:\\Minkin";
         FindDuplicates findDuplicates = new FindDuplicates();
         findDuplicates.findDuplicates(startPath);
-        System.out.println(findDuplicates.findDuplicates(startPath));
+//        System.out.println(findDuplicates.findDuplicates(startPath));
     }
 
     //переменные класса
@@ -23,17 +23,16 @@ public class FindDuplicates {
 
     //
     public List<List<String>> findDuplicates(String startPath) {
-        List<List<String>> resultList = new ArrayList<>();
         try {
             Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<>() {
                 @Override
                 public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
                     FileInfo fileInfo = new FileInfo(path);
 //                System.out.println(fileInfo);
-                    if (filesList.contains(fileInfo)) {
-                        duplicatesSet.add(fileInfo);    //Отдельное множество для повторяющихся
+                    if (filesList.contains(fileInfo)) {  //Проверка - повторяется ли файл в основном списке
+                        duplicatesSet.add(fileInfo);    //Отдельное множество для повторяющихся, т.к. интересны только имена и др. повторяющихся
                     }
-                    filesList.add(fileInfo);        //Собираю список всех файлов
+                    filesList.add(fileInfo);        //Общий список всех файлов
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -47,26 +46,25 @@ public class FindDuplicates {
             System.out.println(exception.getMessage());
         }
 
-        filesList.retainAll(duplicatesSet);
-//        for (FileInfo fileInfo:filesList){
-//            resultList.add(fileInfo.getFileList());
-//        }
+        filesList.retainAll(duplicatesSet);     //Исключаю не повторяющиеся файлы
+        return getResultList();
+    }
+
+    //Метод для формирования итогового списка
+    public List<List<String>> getResultList(){
+        List<List<String>> resultList = new ArrayList<>();
         for(FileInfo fileInfo:duplicatesSet){
-            List<String> duplicatedFilesString = new ArrayList<>();
-            for(FileInfo duplicateFromList:filesList){
+            List<String> duplicatedFilesString = new ArrayList<>();     //Создание списков путей одинаковых файлов
+                                                                        //Количество списков определяется количеством элементов в SET
+            for(FileInfo duplicateFromList:filesList){                  //Списки путей одинаковых файлов формируются путем перебора и копирования из общего списка
                 if(duplicateFromList.equals(fileInfo)){
                     duplicatedFilesString.add(duplicateFromList.file_path.toString());
                 }
             }
             resultList.add(duplicatedFilesString);
         }
-
         return resultList;
     }
-
-//    public File getNextFile(Path path){
-//        return
-//    }
 
 
 }
